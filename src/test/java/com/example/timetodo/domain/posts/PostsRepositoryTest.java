eadc59e4f7a7af5e6747841dcc33669487eb9a5e
Kooky <kooky1001@gmail.com>
2024-01-19 00:12:01 +0900

    BaseTimeEntity 추가

--- src/test/java/com/example/timetodo/domain/posts/PostsRepositoryTest.java
+++ src/test/java/com/example/timetodo/domain/posts/PostsRepositoryTest.java
@@ -6,6 +6,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.*;
@@ -40,6 +41,30 @@ class PostsRepositoryTest {
         Posts posts = postsList.get(0);
         assertThat(posts.getTitle()).isEqualTo(title);
         assertThat(posts.getContent()).isEqualTo(content);
+    }
+
+    @Test
+    void baseTimeEntityTest() {
+        //given
+        LocalDateTime now = LocalDateTime.now();
+        postsRepository.save(Posts.builder()
+                .title("title")
+                .content("content")
+                .author("author")
+                .build());
+
+        //when
+        List<Posts> postsList = postsRepository.findAll();
+
+        //then
+        Posts posts = postsList.get(0);
+
+        System.out.println("now = " + now);
+        System.out.println("posts.getCreatedDate() = " + posts.getCreatedDate());
+        System.out.println("posts.getModifiedDate() = " + posts.getModifiedDate());
+
+        assertThat(posts.getCreatedDate()).isAfter(now);
+        assertThat(posts.getModifiedDate()).isAfter(now);
     }
 
 }
