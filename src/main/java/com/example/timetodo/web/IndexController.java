071f9c2f71c9e2185f513ee3ff4b6213e5fe36df
Kooky <kooky1001@gmail.com>
2024-01-23 14:28:48 +0900

    로그인 세션 어노테이션으로 변경

--- src/main/java/com/example/timetodo/web/IndexController.java
+++ src/main/java/com/example/timetodo/web/IndexController.java
@@ -1,5 +1,6 @@
 package com.example.timetodo.web;
 
+import com.example.timetodo.config.auth.LoginUser;
 import com.example.timetodo.config.auth.dto.SessionUser;
 import com.example.timetodo.domain.user.User;
 import com.example.timetodo.service.posts.PostsService;
@@ -16,13 +17,11 @@ import org.springframework.web.bind.annotation.PathVariable;
 public class IndexController {
 
     private final PostsService postsService;
-    private final HttpSession httpSession;
 
     @GetMapping("/")
-    public String index(Model model) {
+    public String index(Model model, @LoginUser SessionUser user) {
         model.addAttribute("posts", postsService.findAllDesc());
 
-        SessionUser user = (SessionUser) httpSession.getAttribute("user");
         if (user != null) {
             model.addAttribute("userName", user.getName());
         }
